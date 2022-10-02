0a781346f06537ca83e95f402c2365b745dfcc95
kolya5544 <kolya@iktm.me>
2022-10-02 15:26:58 +0300

    Implemented basic interface, implemented Remove beats

--- Program.cs
+++ Program.cs
@@ -1,2 +1,148 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using NAudio.Wave;
+
+string Read(string prompt)
+{
+    Console.Write($"{prompt} ");
+    return Console.ReadLine();
+}
+
+Console.WriteLine("========================");
+Console.WriteLine("= Welcome to BeatAudio =");
+Console.WriteLine("=    Made by IKTeam    =");
+Console.WriteLine("========================");
+Console.WriteLine();
+string filename = Read("Enter filename of a file to load, like .mp3, .wav etc:");
+double bpm = double.Parse(Read("Enter BPM of a song:"));
+double offset = double.Parse(Read("Enter beginning offset in ms, default is 0:"));
+
+var afr = new AudioFileReader(filename);
+var sampleProvider = afr.ToSampleProvider();
+var wf = sampleProvider.WaveFormat;
+var sr = wf.SampleRate; // sample rate (per second)
+int channels = wf.Channels;
+
+var spb = 60 / bpm; // seconds per beat
+var sr_a = (int)Math.Round(sr * spb) * channels; // amount of samples per one beat
+
+// ditch the offset!
+var offsetSamples = (int)Math.Round(sr * (offset / 1000));
+var blackhole = new float[offsetSamples * channels]; sampleProvider.Read(blackhole, 0, blackhole.Length);
+
+var sC = (int)Math.Ceiling((bpm / 60) * (afr.TotalTime.TotalSeconds - offset / 1000)); // calculate amount of BEATS there are
+
+Console.WriteLine($"We had found a total of {sC} beats in a {afr.TotalTime.ToString(@"hh\:mm\:ss")} song");
+
+var sampleBuffer = new float[sr_a];
+List<Beat> beatHolder = new List<Beat>(sC);
+for (int i = 0; i < sC; i++) // read all beats
+{
+    var bR = sampleProvider.Read(sampleBuffer, 0, sr_a);
+    for (int z = bR; z < sr_a; z++) // we don't need remainders of the previous beat in the last beat of the song
+    {
+        sampleBuffer[z] = 0;
+    }
+    var beatBuffer = new float[sr_a];
+    sampleBuffer.CopyTo(beatBuffer, 0);
+    beatHolder.Add(new Beat() { samples = beatBuffer, position = i });
+}
+
+Console.WriteLine($"All the beats successfully loaded! Press ENTER to continue.");
+Console.ReadLine();
+
+while (true)
+{
+    Console.Clear();
+    Console.WriteLine($"= BeatAudio by IKTeam =");
+    Console.WriteLine($"-> File loaded: {Path.GetFileName(filename)}");
+    Console.WriteLine($"-> Amount of beats loaded: {beatHolder.Count}{(beatHolder.Count != sC ? $" (originally {sC})" : "")}");
+    Console.WriteLine();
+    Console.WriteLine($"Possible actions:");
+    Console.WriteLine($"1. Remove every Nth beat out of K beats.");
+    Console.WriteLine($"2. Swap Nth beat with Kth beat out of C beats.");
+    Console.WriteLine($"3. Double every Nth beat out of K beats.");
+    Console.WriteLine($"4. Save the resulting file.");
+    Console.WriteLine();
+    string actionString = Read(">");
+    int act = -1;
+    if (!int.TryParse(actionString, out act) || act == -1) continue;
+
+    switch (act)
+    {
+        case 1:
+            Remove_UI(); break;
+        case 2:
+            Swap_UI(); break;
+        case 3:
+            Double_UI(); break;
+        case 4:
+            Save_UI(); break;
+    }
+}
+
+void Remove_UI()
+{
+    Console.WriteLine();
+    Console.WriteLine("= REMOVE GUIDE =");
+    Console.WriteLine("You can split all the beats there are into groups of a fixed size C.");
+    Console.WriteLine("Then, you can remove every Nth beat out of that group.");
+    Console.WriteLine("For example, to remove every other beat, you need a group of 2 beats, and remove every 2nd beat.");
+    Console.WriteLine("If you want to remove the 8th beat and leave all the 7 beats before it intact, you need a group of 8 beats, and remove every 8th beat.");
+    Console.WriteLine();
+    string beatsList = "";
+    List<int> beats = new();
+    while (true)
+    {
+        beatsList = Read("I want to remove the next beats (list of numbers, split with comma, like 1,2,3):");
+        try
+        {
+            beatsList.Split(',').ToList().ForEach(z => beats.Add(int.Parse(z) - 1)); // we do - 1 to account for indexes beginning with 0
+            break;
+        } catch
+        {
+
+        }
+    }
+    int groupSize = 0;
+    while (true)
+    {
+        groupSize = int.Parse(Read($"There should be this amount of beats in a group (at least {beats.Max()}!!):"));
+        if (groupSize >= beats.Count) break;
+    }
+
+    Utils.RemoveNthBeats(ref beatHolder, beats.ToArray(), groupSize);
+
+    Console.WriteLine($"Success! Press Enter to return.");
+    Console.ReadLine();
+}
+
+void Swap_UI()
+{
+
+}
+
+void Double_UI()
+{
+
+}
+
+void Save_UI()
+{
+    Console.WriteLine();
+    string output = Read("Filename of the result file:");
+
+    var afw = new WaveFileWriter(output, new WaveFormat(sr, channels));
+    Console.WriteLine($"Saving the file to {output}... Please wait!");
+    var resultBuffer = new List<float>(beatHolder.Count * sr_a);
+    beatHolder.ForEach((z) => { resultBuffer.AddRange(z.samples); });
+    afw.WriteSamples(resultBuffer.ToArray(), 0, resultBuffer.Count);
+    afw.Flush();
+    afw.Close();
+    Console.WriteLine($"Successfully written {resultBuffer.Count} samples to {output}! Press any key to return to main menu. You can now safely close the program, too.");
+    Console.ReadLine();
+}
+
+public class Beat
+{
+    public int position;
+    public float[] samples;
+}
---
+++ Utils.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+public class Utils
+{
+    public static void RemoveNthBeats(ref List<Beat> beats, int[] toRemove, int beatSampleSize)
+    {
+        for (int i = 0; i < beats.Count; i++)
+        {
+            Beat b = beats[i];
+            if (toRemove.Contains(b.position % beatSampleSize))
+            {
+                beats.Remove(b);
+                i--; continue;
+            }
+        }
+        FixPositionOrder(ref beats);
+    }
+
+    public static void SwapBeats(ref List<Beat> beats, int swapN1, int swapN2, int beatSampleSize)
+    {
+        for (int i = 0; i < (beats.Count / beatSampleSize) + (beats.Count % beatSampleSize == 0 ? 0 : 1); i++)
+        {
+            var batch = beats.GetRange(i * beatSampleSize, Math.Min(beatSampleSize, beats.Count - i * beatSampleSize));
+
+            if (swapN1 < batch.Count && swapN2 < batch.Count)
+            {
+                var l = batch[swapN1].position;
+                batch[swapN1].position = batch[swapN2].position;
+                batch[swapN2].position = l;
+            }
+        }
+        beats = Reorder(beats);
+    }
+
+    public static void ReverseBeats(ref List<Beat> beats, int beatSampleSize = 1)
+    {
+        var margin = (beats.Count / beatSampleSize) + (beats.Count % beatSampleSize == 0 ? 0 : 1);
+        List<Beat> output = new(beats.Count);
+        for (int i = margin - 1; i >= 0; i--)
+        {
+            var batch = beats.GetRange(i * beatSampleSize, Math.Min(beatSampleSize, beats.Count - i * beatSampleSize));
+
+            output.AddRange(batch);
+        }
+        FixPositionOrder(ref output);
+        beats = output;
+    }
+
+    public static void DoubleBeats(ref List<Beat> beats, int[] doubleList, int beatSampleSize)
+    {
+        var limit = (beats.Count / beatSampleSize) + (beats.Count % beatSampleSize == 0 ? 0 : 1);
+        doubleList = doubleList.OrderBy((z) => z).ToArray();
+        List<Beat> output = new(beats.ToArray());
+        int pers = 0;
+        for (int i = 0; i < limit; i++)
+        {
+            var batch = beats.GetRange(i * beatSampleSize, Math.Min(beatSampleSize, beats.Count - i * beatSampleSize));
+
+            for (int z = 0; z < doubleList.Length; z++)
+            {
+                int perm = doubleList[z];
+                if (perm < batch.Count)
+                {
+                    output.Insert(perm + pers + i * beatSampleSize + 1, batch[perm]);
+                    pers += 1;
+                }
+            }
+        }
+        FixPositionOrder(ref output);
+        beats = output;
+    }
+
+    public static List<Beat> Reorder(List<Beat> beats)
+    {
+        beats = beats.OrderBy((z) => z.position).ToList();
+        return beats;
+    }
+
+    public static void FixPositionOrder(ref List<Beat> beats)
+    {
+        for (int i = 0; i < beats.Count; i++)
+        {
+            beats[i].position = i;
+        }
+    }
+}
